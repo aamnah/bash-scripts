2c6b7c56f83229aa03ff406f5e826f16ac086f4c
Aamnah Akram <aamnah@aamnah.com>
2016-02-05 15:23:57 +0500

    updated logs directory
    removed ${APACHE_LOG_DIR} and added /var/www/$1/logs/ directory for logs

--- create_virtualhost.sh
+++ create_virtualhost.sh
@@ -1,8 +1,9 @@
 # create_virtualHosts
 # derived from: https://www.digitalocean.com/community/tutorials/how-to-set-up-apache-virtual-hosts-on-ubuntu-14-04-lts
 
-# create dir
+# create dirs
 mkdir -p /var/www/$1/public_html
+mkdir -p /var/www/$1/logs
 
 # grant perms
 sudo chown -R $USER:$USER /var/www/$1/public_html
@@ -30,8 +31,8 @@ echo -e "<VirtualHost *:80>
     ServerName $1
     ServerAlias www.$1
     DocumentRoot /var/www/$1/public_html/
-    ErrorLog ${APACHE_LOG_DIR}/error.log
-    CustomLog ${APACHE_LOG_DIR}/access.log combined
+    ErrorLog /var/www/$1/logs/error.log
+    CustomLog /var/www/$1/logs/access.log combined
 </VirtualHost>" > /etc/apache2/sites-available/$1.conf
 
 # enable new virtual host file
