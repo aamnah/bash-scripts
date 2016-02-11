2fef4f61500c3a0bea8373247f3c421622f8c500
Aamnah Akram <aamnah@aamnah.com>
2016-02-11 16:06:13 +0500

    Create install_webmin_debian.sh

---
+++ install_webmin_debian.sh
@@ -0,0 +1,22 @@
+# Install Webmin on Debian
+
+# add the Webmin repository to `sources.list`
+sudo echo "
+# Webmin
+deb http://download.webmin.com/download/repository sarge contrib
+deb http://webmin.mirror.somersettechsolutions.co.uk/repository sarge contrib
+" >> /etc/apt/sources.list
+
+# add Webmin GPG key to apt, so the source repo will be trusted
+wget -q http://www.webmin.com/jcameron-key.asc -O- | sudo apt-key add -
+
+sudo apt-get update
+sudo apt-get install webmin -y
+
+# Add UFW Firewall Rule for Webmin port
+# sudo ufw allow 10000/tcp
+sudo ufw allow webmin
+
+# default port: 10000
+# default login: the server login of the user who installed
+# https://server_IP_address:10000
