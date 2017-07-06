88b48b72222d9ba1c1ca024dab1b8971162dc7cb
Aamnah Akram <aamnah@users.noreply.github.com>
2017-07-06 14:38:21 +0500

    added mbstring php module
    mbstring is required for PHPMyAdmin

--- install_lamp_debian.sh
+++ install_lamp_debian.sh
@@ -18,10 +18,10 @@ apt-get install apache2 apache2-doc apache2-utils libexpat1 ssl-cert -y
 
 # PHP and Modules
 # PHP5
-# apt-get install php5.6 libapache2-mod-php5.6 php5.6-cli php5.6-common php5.6-curl php5.6-dev php5.6-gd php5.6-intl php5.6-mcrypt php5.6-mysql  php5.6-recode php5.6-xsl php5.6-pspell php5.6-ps php5.6-imagick php-pear -y
+# apt-get install php5.6 libapache2-mod-php5.6 php5.6-cli php5.6-common php5.6-curl php5.6-dev php5.6-gd php5.6-intl php5.6-mcrypt php5.6-mbstring php5.6-mysql  php5.6-recode php5.6-xsl php5.6-pspell php5.6-ps php5.6-imagick php-pear -y
 
 # PHP7
-apt-get install php7.0 libapache2-mod-php7.0 php7.0-cli php7.0-common php7.0-curl php7.0-dev php7.0-gd php7.0-intl php7.0-mcrypt php7.0-mysql php7.0-pspell php7.0-recode php7.0-xsl php-imagick php-pear -y
+apt-get install php7.0 libapache2-mod-php7.0 php7.0-cli php7.0-common php7.0-curl php7.0-dev php7.0-gd php7.0-intl php7.0-mcrypt php7.0-mbstring php7.0-mysql php7.0-pspell php7.0-recode php7.0-xsl php-imagick php-pear -y
 
 # MySQL
 # will prompt for password
@@ -39,8 +39,8 @@ chown -R www-data:www-data /var/www
 
 # Enable mod_rewrite, required for WordPress permalinks and .htaccess files
 a2enmod rewrite
-# phpenmod -v 5.6 mcrypt # PHP5
-phpenmod -v 7.0 mcrypt # PHP7
+# phpenmod -v 5.6 mcrypt mbstring # PHP5
+phpenmod -v 7.0 mcrypt mbstring # PHP7
 
 # Restart Apache
 service apache2 restart
