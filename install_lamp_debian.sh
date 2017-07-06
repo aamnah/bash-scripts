03781293ebfa308807d026debf8893ad2241fba9
Aamnah Akram <aamnah@users.noreply.github.com>
2017-07-06 13:31:54 +0500

    updated php5 modules

--- install_lamp_debian.sh
+++ install_lamp_debian.sh
@@ -18,7 +18,7 @@ apt-get install apache2 apache2-doc apache2-utils libexpat1 ssl-cert -y
 
 # PHP and Modules
 # PHP5
-# apt-get install php5 libapache2-mod-php5 php5-cli php5-common php5-curl php5-dev php5-gd php5-intl php5-mcrypt php5-mysql  php5-recode php5-xsl php5-pspell php5-ps php5-imagick php-pear -y
+# apt-get install php5.6 libapache2-mod-php5.6 php5.6-cli php5.6-common php5.6-curl php5.6-dev php5.6-gd php5.6-intl php5.6-mcrypt php5.6-mysql  php5.6-recode php5.6-xsl php5.6-pspell php5.6-ps php5.6-imagick php-pear -y
 
 # PHP7
 apt-get install php7.0 libapache2-mod-php7.0 php7.0-cli php7.0-common php7.0-curl php7.0-dev php7.0-gd php7.0-intl php7.0-mcrypt php7.0-mysql php7.0-pspell php7.0-recode php7.0-xsl php-imagick php-pear -y
