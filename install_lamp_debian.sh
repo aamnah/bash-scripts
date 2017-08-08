2d43c89fbc170e6723f018856583e4d1bddce23b
Aamnah Akram <aamnah@users.noreply.github.com>
2017-08-08 23:19:45 +0500

    just install php-exts without mentioning version

--- install_lamp_debian.sh
+++ install_lamp_debian.sh
@@ -27,7 +27,8 @@ apt install apache2 apache2-doc apache2-utils libexpat1 ssl-cert -y
 # apt install php5.6 libapache2-mod-php5.6 php5.6-cli php5.6-common php-curl php5.6-curl php5.6-dev php5.6-gd php5.6-intl php5.6-mcrypt php5.6-mbstring php5.6-mysql php5.6-recode php5.6-xml php5.6-pspell php5.6-ps php5.6-imagick php-pear php-gettext -y
 
 # PHP7
-apt install php php7.1-common php-curl php7.1-curl php7.1-dev php7.1-gd php7.1-intl php7.1-mcrypt php7.1-mbstring php7.1-mysql php7.1-pspell php7.1-recode php7.1-xml php-imagick php-pear php-gettext -y
+# apt install php php7.1-common php-curl php7.1-curl php7.1-dev php7.1-gd php7.1-intl php7.1-mcrypt php7.1-mbstring php7.1-mysql php7.1-pspell php7.1-recode php7.1-xml php-imagick php-pear php-gettext -y
+apt install php php-common php-curl php-dev php-gd php-gettext php-imagick php-intl php-mbstring php-mcrypt php-mysql php-pear php-pspell php-recode php-xml -y
 
 # MySQL
 # will prompt for password
