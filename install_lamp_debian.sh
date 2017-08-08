9fb5f193ee75826b00e415dfcffb9c24f57d354d
Aamnah Akram <aamnah@users.noreply.github.com>
2017-08-08 12:16:24 +0500

    xml, not xsl

--- install_lamp_debian.sh
+++ install_lamp_debian.sh
@@ -19,15 +19,15 @@ apt install apache2 apache2-doc apache2-utils libexpat1 ssl-cert -y
 # PHP and Modules
 
 # PHP5 on Ubuntu 14.04 LTS
-# apt install php5 libapache2-mod-php5 php5-cli php5-common php5-curl php5-dev php5-gd php5-intl php5-mcrypt php5-mysql php5-recode php5-xsl php5-pspell php5-ps php5-imagick php-pear php-gettext -y
+# apt install php5 libapache2-mod-php5 php5-cli php5-common php5-curl php5-dev php5-gd php5-intl php5-mcrypt php5-mysql php5-recode php5-xml php5-pspell php5-ps php5-imagick php-pear php-gettext -y
 
 # PHP5 on Ubuntu 17.04 Zesty
 # sudo add-apt-repository ppa:ondrej/php
 # sudo apt update
-# apt install php5.6 libapache2-mod-php5.6 php5.6-cli php5.6-common php-curl php5.6-curl php5.6-dev php5.6-gd php5.6-intl php5.6-mcrypt php5.6-mbstring php5.6-mysql php5.6-recode php5.6-xsl php5.6-pspell php5.6-ps php5.6-imagick php-pear php-gettext -y
+# apt install php5.6 libapache2-mod-php5.6 php5.6-cli php5.6-common php-curl php5.6-curl php5.6-dev php5.6-gd php5.6-intl php5.6-mcrypt php5.6-mbstring php5.6-mysql php5.6-recode php5.6-xml php5.6-pspell php5.6-ps php5.6-imagick php-pear php-gettext -y
 
 # PHP7
-apt install php php7.1-common php-curl php7.1-curl php7.1-dev php7.1-gd php7.1-intl php7.1-mcrypt php7.1-mbstring php7.1-mysql php7.1-pspell php7.1-recode php7.1-xsl php-imagick php-pear php-gettext -y
+apt install php php7.1-common php-curl php7.1-curl php7.1-dev php7.1-gd php7.1-intl php7.1-mcrypt php7.1-mbstring php7.1-mysql php7.1-pspell php7.1-recode php7.1-xml php-imagick php-pear php-gettext -y
 
 # MySQL
 # will prompt for password
