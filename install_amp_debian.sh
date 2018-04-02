6969697fce9f1c59c8e897401024d5bcdb094065
Aamnah Akram <aamnah@users.noreply.github.com>
2018-04-02 19:30:54 +0500

    updated response message and todo

--- install_amp_debian.sh
+++ install_amp_debian.sh
@@ -42,7 +42,7 @@ installApache() {
 
 installLetsEncryptCertbot() {
   # Let's Encrypt SSL 
-  echo -e "\n ${Cyan} Let's Encrypt SSL.. ${Color_Off}"
+  echo -e "\n ${Cyan} Installing Let's Encrypt SSL.. ${Color_Off}"
 
   apt update # update repo sources
   apt install software-properties-common -y # required in order to add a repo
@@ -147,7 +147,8 @@ echo -e "\n${Green} SUCCESS! MySQL password is: ${PASS_MYSQL_ROOT} ${Color_Off}"
 
 # TODO
 # - [x] Figure out why it is asking for MySQL password and not just taking it from the variable in heredoc (cz: to avoid redirection, programs don't let heredoc enter passwords)
-# - [ ] Figure out a secure way for entering MySQL password (where it isn't entered on command prompt and saved in bash history as a result)
+# - [ ] Figure out a secure way for entering MySQL password (where it isn't shown on command prompt and saved in bash history as a result)
+# - [ ] Prompt or indicate somehow that MySQL password won't be overwritten if already set
 # - [ ] Email password (or add it to MOTD)
 
 # LINKS
