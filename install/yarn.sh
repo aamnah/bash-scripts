9e9b59d3e30bec52a661d11e9a125723e929e380
Aamnah <hello@aamnah.com>
2019-03-15 11:22:07 +0500

    initial

---
+++ install/yarn.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+install_yarn () {
+  echo -e "\n  Adding Yarn repo (Stable) .."
+  # add GPG key
+  curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+  
+  # add repo (Stable 1.13.0)
+  echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+  
+  # update sources
+  sudo apt update
+
+  echo -e "\n  Installing Yarn .."
+  # since using nvm, avoid the node installation with `--no-install-recommends`
+  sudo apt install --no-install-recommends yarn
+}
+
+
+install_yarn
+
+echo -e "\n  DONE. $(yarn --version) is installed. \n"
+
+
+
+
+# LINKS
+# https://yarnpkg.com/en/docs/install#debian-stable
