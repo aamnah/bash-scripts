749ef8033db4e970faeeef6f643512f8f4e20a18
Aamnah <hello@aamnah.com>
2019-03-15 11:32:12 +0500

    initial

---
+++ install/nvm.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+uninstall_node () {
+  echo -e "\n\n Removig existing Nodejs, if any .."
+  # apt packages
+  sudo apt purge --auto-remove nodejs npm
+  
+  # snap packages
+  sudo snap remove node npm
+
+  # remove node source from /etc/apt/sources.list.d
+  sudo rm -rf /etc/apt/sources.list.d/nodesource.list
+  sudo rm -rf /etc/apt/sources.list.d/nodesource.list.save
+
+  # update
+  sudo apt update
+}
+
+install_nvm () {
+  echo -e "\n\n ----- Installing NVM (Node Version Manager) .."
+  wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.34.0/install.sh | bash
+
+  # make sure the source lines added to `~/.bashrc` are loaded in current terminal
+  # otherwise the rest of the script will give a 'nvm: command not found' error
+  source ~/.bashrc
+
+  # confirm nvm install
+  echo -e "\n\n ----- NVM $(nvm --version) is installed .. "
+
+  # install the node versions
+  # nvm install node # install Latest
+  nvm install --lts # install LTS version
+
+  # select the version you want to use
+  # nvm use node # use Latest
+  # nvm use --lts # use LTS version
+
+  # installing a version for the first time also uses and sets it as default
+
+  # exit successfully
+  exit 0
+}
+
+# uninstall_node
+install_nvm
+
+
+# TODO
+# CHECK IF NODE IS INSTALLED BEFORE TRYING TO REMOVE IT
+# Check if the bash profile is called `.bashrc` (Ubuntu) or `.bash_profile` (macOS)
