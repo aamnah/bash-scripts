c13d2494c0d88e628b88f46b0927f611ce2897e1
Aamnah <hello@aamnah.com>
2019-03-21 07:58:19 +0500

    initial

---
+++ setup/dev_ubuntu.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+sudo apt update
+
+# Browsers
+sudo snap install opera chromium
+
+# sublime text
+sudo snap install sublime-text --classic
+sudo apt install -y libgtk2.0-0
+
+# visual studio code
+sudo snap install vscode --classic
+
+# slack
+sudo snap install slack --classic
+
+# tldr (community-driven man pages)
+# https://tldr.sh/
+sudo snap install tldr
